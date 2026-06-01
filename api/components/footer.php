849e772e6c9ea6ba4cb90678c7a91986244c867a
Jinat Sahorior <sahoriorjinat4@gmail.com>
2026-06-01 23:24:47 +0530

    Create footer.php

---
+++ api/components/footer.php
@@ -0,0 +1,8 @@
+<?php // api/components/footer.php ?>
+    </main>
+
+    <footer class="border-t border-gray-900 bg-gray-950 text-center py-6 text-xs text-gray-500">
+        &copy; <?php echo date('Y'); ?> <?php echo $community['name']; ?>. All rights reserved.
+    </footer>
+</body>
+</html>
